04ce8992e802a2ba8069e7cbab0e7c894ec61ea5
yaroslav_kush <yaroslav_kush@epam.com>
2019-07-11 17:00:19 +0300

    Part of feature - 1

--- src/main/java/app/servlets/AddServlet.java
+++ src/main/java/app/servlets/AddServlet.java
@@ -23,8 +23,6 @@ public class AddServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         String password = req.getParameter("password");
-        String password2 = req.getParameter("password");
-        String password5 = req.getParameter("password");
         User user = new User(name, password);
         User user1 = new User(name, password);
         User user2 = new User(name, password);
